92904cfdba87bdf5ff02ae0dbbda9e5feeaa1aa2
CristalTs <cristaltshishimbi@yahoo.fr>
2018-10-27 19:37:07 -0200

    primeiro commit

---
+++ src/test/java/com/cristal/primeiroapi/P20ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cristal.primeiroapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class P20ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
